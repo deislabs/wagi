0bc349ad23e70d1b6d16c018d81e7cf078ce5bfa
Matt Butcher <matt.butcher@microsoft.com>
2021-06-01 21:14:02 -0600

    Support multiple modules per Bindle
    Signed-off-by: Matt Butcher <matt.butcher@microsoft.com>

--- examples/mkbindle.rs
+++ examples/mkbindle.rs
@@ -5,19 +5,24 @@
 ///!
 ///! Note that if you change either file there, you might need to recheck the
 ///! size and SHA256 sum of the hello.wasm. Use `shasum -a 256 hello.wasm`
-use sha2::Digest;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     // Start with this invoice
     let data = std::fs::read_to_string("examples/invoice.toml")?;
     let inv: bindle::Invoice = toml::from_str(data.as_str())?;
+    let bindle_server = std::env::var("BINDLE_SERVER_URL")
+        .unwrap_or_else(|_| "http://127.0.0.1:8080/v1".to_owned());
 
     // Connect to our Bindle server
-    let bindler = bindle::client::Client::new("http://localhost:8080/v1")?;
+    let bindler = bindle::client::Client::new(bindle_server.as_str())?;
 
     // Create the invoice
-    let icr = bindler.create_invoice(inv).await?;
+    let iid = inv.bindle.id.clone();
+    let icr = bindler
+        .create_invoice(inv)
+        .await
+        .map_err(|e| anyhow::anyhow!("Failed to create invoice for {}: {}", iid, e))?;
 
     // For every missing item, look for a local parcel and upload it.
     if let Some(items) = icr.missing {
@@ -32,22 +37,7 @@ async fn main() -> Result<(), anyhow::Error> {
         }
     }
 
-    println!("You can now use example.com/hello/1.0.0");
+    println!("You can now use {}", iid);
 
     Ok(())
 }
-
-// This basically replicates shasum -a 256 examples/hello.wasm
-/*
-async fn hash_file() {
-    let mut file = tokio::fs::File::open("examples/hello.wasm")
-        .await
-        .expect("file cannot be opened");
-    let mut hasher = bindle::async_util::AsyncSha256::new();
-    tokio::io::copy(&mut file, &mut hasher)
-        .await
-        .expect("hashing file failed");
-    let sha = format!("{:x}", hasher.into_inner().unwrap().finalize());
-    print!("Hash was {}", sha);
-}
-*/
