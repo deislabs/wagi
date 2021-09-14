1a1785f345aed2b591e6a13815afc0f8e5550835
itowlson <ivan.towlson@microsoft.com>
2021-09-15 07:49:24 +1200

    Interim commit

--- src/wagi_config.rs
+++ src/wagi_config.rs
@@ -1,5 +1,7 @@
 use std::{collections::HashMap, net::SocketAddr, path::PathBuf};
 
+use serde::Deserialize;
+
 #[derive(Clone, Debug)]
 pub struct WagiConfiguration {
     pub handlers: HandlerConfigurationSource,
@@ -28,4 +30,24 @@ pub struct HttpConfiguration {
 pub struct TlsConfiguration {
     pub cert_path: PathBuf,
     pub key_path: PathBuf,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct ModuleMapConfiguration {
+    #[serde(rename = "module")]
+    pub entries: Vec<ModuleMapConfigurationEntry>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct ModuleMapConfigurationEntry {
+    // The route to wire up
+    pub route: String,
+    // The Wasm to wire it up to
+    pub module: String,  // file path, file://foo URL, bindle:foo/bar/1.2.3 or oci:foo/bar:1.2.3 (bindle: is deprecated which is good because it's not clear which parcel you'd use)
+    pub entrypoint: Option<String>,
+    pub bindle_server: Option<String>,
+    // The environment in which to run it
+    pub volumes: Option<HashMap<String, String>>,
+    pub allowed_hosts: Option<Vec<String>>,
+    pub http_max_concurrency: Option<u32>,
 }
