7e1ed21c24c7ac7542c512deb2a5e6dcf6860f6c
itowlson <ivan.towlson@microsoft.com>
2021-09-20 10:10:55 +1200

    Move compose_response to dispatcher for now

--- src/dispatcher.rs
+++ src/dispatcher.rs
@@ -4,8 +4,9 @@ use std::sync::{Arc, RwLock};
 
 use cap_std::fs::Dir;
 use hyper::{
+    http::header::{HeaderName, HeaderValue},
     http::request::Parts,
-    Body, Response,
+    Body, Response, StatusCode,
 };
 use sha2::{Digest, Sha256};
 use tracing::{debug, instrument};
@@ -14,6 +15,7 @@ use wasi_common::pipe::{ReadPipe, WritePipe};
 use wasmtime::*;
 use wasmtime_wasi::*;
 
+use crate::http_util::{internal_error, parse_cgi_headers};
 use crate::request::{RequestContext, RequestGlobalContext, RequestRouteContext};
 
 use bindle::Invoice;
@@ -207,7 +209,7 @@ impl WasmRouteHandler {
 
         self.run_prepared_wasm_instance(routing_info, route_context, instance, store)?;
 
-        crate::runtime::compose_response(redirects.stdout_mutex)
+        compose_response(redirects.stdout_mutex)
     }
 
     fn prepare_stdio_streams(body: Vec<u8>, global_context: &RequestGlobalContext, handler_id: String) -> Result<crate::wasm_module::IORedirectionInfo, Error> {
@@ -399,6 +401,92 @@ impl RoutingTable {
             RoutingTableEntry::inbuilt("/healthz", RouteHandler::HealthCheck),
         ]
     }
+}
+
+// TODO: NOT HERE
+pub fn compose_response(stdout_mutex: Arc<RwLock<Vec<u8>>>) -> Result<Response<Body>, Error> {
+    // Okay, once we get here, all the information we need to send back in the response
+    // should be written to the STDOUT buffer. We fetch that, format it, and send
+    // it back. In the process, we might need to alter the status code of the result.
+    //
+    // This is a little janky, but basically we are looping through the output once,
+    // looking for the double-newline that distinguishes the headers from the body.
+    // The headers can then be parsed separately, while the body can be sent back
+    // to the client.
+
+    let out = stdout_mutex.read().unwrap();
+    let mut last = 0;
+    let mut scan_headers = true;
+    let mut buffer: Vec<u8> = Vec::new();
+    let mut out_headers: Vec<u8> = Vec::new();
+    out.iter().for_each(|i| {
+        if scan_headers && *i == 10 && last == 10 {
+            out_headers.append(&mut buffer);
+            buffer = Vec::new();
+            scan_headers = false;
+            return; // Consume the linefeed
+        }
+        last = *i;
+        buffer.push(*i)
+    });
+    let mut res = Response::new(Body::from(buffer));
+    let mut sufficient_response = false;
+    parse_cgi_headers(String::from_utf8(out_headers)?)
+        .iter()
+        .for_each(|h| {
+            use hyper::header::{CONTENT_TYPE, LOCATION};
+            match h.0.to_lowercase().as_str() {
+                "content-type" => {
+                    sufficient_response = true;
+                    res.headers_mut().insert(CONTENT_TYPE, h.1.parse().unwrap());
+                }
+                "status" => {
+                    // The spec does not say that status is a sufficient response.
+                    // (It says that it may be added along with Content-Type, because
+                    // a status has a content type). However, CGI libraries in the wild
+                    // do not set content type correctly if a status is an error.
+                    // See https://datatracker.ietf.org/doc/html/rfc3875#section-6.2
+                    sufficient_response = true;
+                    // Status can be `Status CODE [STRING]`, and we just want the CODE.
+                    let status_code = h.1.split_once(' ').map(|(code, _)| code).unwrap_or(h.1);
+                    tracing::debug!(status_code, "Raw status code");
+                    match status_code.parse::<StatusCode>() {
+                        Ok(code) => *res.status_mut() = code,
+                        Err(e) => {
+                            tracing::log::warn!("Failed to parse code: {}", e);
+                            *res.status_mut() = StatusCode::BAD_GATEWAY;
+                        }
+                    }
+                }
+                "location" => {
+                    sufficient_response = true;
+                    res.headers_mut()
+                        .insert(LOCATION, HeaderValue::from_str(h.1).unwrap());
+                    *res.status_mut() = StatusCode::from_u16(302).unwrap();
+                }
+                _ => {
+                    // If the header can be parsed into a valid HTTP header, it is
+                    // added to the headers. Otherwise it is ignored.
+                    match HeaderName::from_lowercase(h.0.as_str().to_lowercase().as_bytes()) {
+                        Ok(hdr) => {
+                            res.headers_mut()
+                                .insert(hdr, HeaderValue::from_str(h.1).unwrap());
+                        }
+                        Err(e) => {
+                            tracing::error!(error = %e, header_name = %h.0, "Invalid header name")
+                        }
+                    }
+                }
+            }
+        });
+    if !sufficient_response {
+        return Ok(internal_error(
+            // Technically, we let `status` be sufficient, but this is more lenient
+            // than the specification.
+            "Exactly one of 'location' or 'content-type' must be specified",
+        ));
+    }
+    Ok(res)
 }
 
 #[cfg(test)]
