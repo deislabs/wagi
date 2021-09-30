f470ed1ea4213cdca99f74abd8589aff3609fc5e
itowlson <ivan.towlson@microsoft.com>
2021-10-01 12:22:25 +1300

    Tests for dynamic routes

--- src/lib.rs
+++ src/lib.rs
@@ -110,6 +110,40 @@ mod test {
         .expect("Error producing HTTP response")
     }
 
+    async fn get_plain_text_response_from_standalone_bindle(bindle_id: &str, route: &str) -> String {
+        let empty_body = hyper::body::Body::empty();
+        let uri = format!("http://127.0.0.1:3000{}", route);
+        let request = hyper::Request::get(&uri).body(empty_body);
+
+        let response = send_request_to_standalone_bindle(bindle_id, request).await;
+
+        assert_eq!(hyper::StatusCode::OK, response.status());
+
+        // Content-Type could include a charset
+        let content_type = response.headers().get("Content-Type").expect("Expected Content-Type header").to_str().unwrap();
+        assert!(content_type.starts_with("text/plain"));
+
+        let response_body = hyper::body::to_bytes(response.into_body()).await
+            .expect("Could bot get bytes from response body");
+        let response_text = std::str::from_utf8(&response_body)
+            .expect("Could not read body as string");
+        response_text.to_owned()
+    }
+
+    async fn get_decription_and_evs_from_standalone_bindle(bindle_id: &str, route: &str) -> (String, HashMap<String, String>) {
+        let response_text = get_plain_text_response_from_standalone_bindle(bindle_id, route).await;
+
+        let description = response_text.lines().nth(0).unwrap().to_owned();
+        
+        let env_vars = response_text
+            .lines()
+            .skip(1)
+            .filter_map(|line| parse_ev_line(line))
+            .collect::<HashMap<_, _>>();
+
+        (description, env_vars)
+    }
+
     // TODO: set up more specific and focused WASM apps that allow better testing
     // of routes, headers, EVs, and responses.
 
@@ -155,28 +189,17 @@ mod test {
         assert_eq!(hyper::StatusCode::NOT_FOUND, response.status());
     }
 
-    fn parse_ev_line(line: &str) -> Option<(&str, &str)> {
+    fn parse_ev_line(line: &str) -> Option<(String, String)> {
         line.find('=').and_then(|index| {
             let left = &line[..index];
             let right = &line[(index + 2)..];
-            Some((left.trim(), right.trim()))
+            Some((left.trim().to_owned(), right.trim().to_owned()))
         })
     }
 
     #[tokio::test]
     pub async fn http_settings_are_mapped_to_env_vars() {
-        let empty_body = hyper::body::Body::empty();
-        let request = hyper::Request::get("http://127.0.0.1:3000/").body(empty_body);
-
-        let response = send_request_to_standalone_bindle(PRINT_ENV_SA_ID, request).await;
-
-        assert_eq!(hyper::StatusCode::OK, response.status());
-        assert_eq!("text/plain", response.headers().get("Content-Type").expect("Expected Content-Type header"));
-
-        let response_body = hyper::body::to_bytes(response.into_body()).await
-            .expect("Could bot get bytes from response body");
-        let response_text = std::str::from_utf8(&response_body)
-            .expect("Could not read body as string");
+        let response_text = get_plain_text_response_from_standalone_bindle(PRINT_ENV_SA_ID, "/").await;
         let parsed_response = response_text
             .lines()
             .filter_map(|line| parse_ev_line(line))
@@ -193,18 +216,7 @@ mod test {
 
     #[tokio::test]
     pub async fn http_settings_are_mapped_to_env_vars_wildcard_route() {
-        let empty_body = hyper::body::Body::empty();
-        let request = hyper::Request::get("http://127.0.0.1:3000/test/fizz/buzz").body(empty_body);
-
-        let response = send_request_to_standalone_bindle(PRINT_ENV_SA_ID, request).await;
-
-        assert_eq!(hyper::StatusCode::OK, response.status());
-        assert_eq!("text/plain", response.headers().get("Content-Type").expect("Expected Content-Type header"));
-
-        let response_body = hyper::body::to_bytes(response.into_body()).await
-            .expect("Could bot get bytes from response body");
-        let response_text = std::str::from_utf8(&response_body)
-            .expect("Could not read body as string");
+        let response_text = get_plain_text_response_from_standalone_bindle(PRINT_ENV_SA_ID, "/test/fizz/buzz").await;
         let parsed_response = response_text
             .lines()
             .filter_map(|line| parse_ev_line(line))
@@ -217,5 +229,119 @@ mod test {
         assert_eq!("/test", parsed_response["SCRIPT_NAME"]);
         assert_eq!("123.4.5.6", parsed_response["REMOTE_ADDR"]);
         assert_eq!("GET", parsed_response["REQUEST_METHOD"]);
+    }
+
+    #[tokio::test]
+    pub async fn dynamic_routes_set_path_env_vars_correctly() {
+        let bindle_id = DYNAMIC_ROUTES_SA_ID;
+
+        {
+            let route = "/";
+
+            let (description, parsed_response) = get_decription_and_evs_from_standalone_bindle(bindle_id, route).await;
+            assert_eq!("This is the main entry point", description);
+            
+            assert_eq!("", parsed_response["PATH_INFO"]);
+            assert_eq!("", parsed_response["PATH_TRANSLATED"]);
+            assert_eq!("/", parsed_response["X_MATCHED_ROUTE"]);
+            assert_eq!("", parsed_response["X_RAW_PATH_INFO"]);
+            assert_eq!("/", parsed_response["SCRIPT_NAME"]);
+        }
+
+        {
+            let route = "/exactparent";
+
+            let (description, parsed_response) = get_decription_and_evs_from_standalone_bindle(bindle_id, route).await;
+            assert_eq!("This is the main entry point", description);
+            
+            assert_eq!("", parsed_response["PATH_INFO"]);
+            assert_eq!("", parsed_response["PATH_TRANSLATED"]);
+            assert_eq!("/exactparent", parsed_response["X_MATCHED_ROUTE"]);
+            assert_eq!("", parsed_response["X_RAW_PATH_INFO"]);
+            assert_eq!("/exactparent", parsed_response["SCRIPT_NAME"]);
+        }
+
+        {
+            // In a _routes scenario, it is not clear how to canonically parse the URL.
+            // We choose treat the CGI script name as the *full* routing path, i.e. the
+            // *concatenation* of the static parent path and the dynamic _routes-supplied
+            // subpath. This is not currently specified by the WAGI spec, though, and earlier
+            // implementations of Rust WAGI handled it differently!
+            let route = "/exactparent/exact";
+
+            let (description, parsed_response) = get_decription_and_evs_from_standalone_bindle(bindle_id, route).await;
+            assert_eq!("This is the .../exact handler", description);
+            
+            assert_eq!("", parsed_response["PATH_INFO"]);
+            assert_eq!("", parsed_response["PATH_TRANSLATED"]);
+            assert_eq!("/exactparent/exact", parsed_response["X_MATCHED_ROUTE"]);
+            assert_eq!("", parsed_response["X_RAW_PATH_INFO"]);
+            assert_eq!("/exactparent/exact", parsed_response["SCRIPT_NAME"]);
+        }
+
+        {
+            let route = "/exactparent/wildcard/fizz/buzz";
+
+            let (description, parsed_response) = get_decription_and_evs_from_standalone_bindle(bindle_id, route).await;
+            assert_eq!("This is the .../wildcard/... handler", description);
+            
+            assert_eq!("/fizz/buzz", parsed_response["PATH_INFO"]);
+            assert_eq!("/fizz/buzz", parsed_response["PATH_TRANSLATED"]);
+            assert_eq!("/exactparent/wildcard/...", parsed_response["X_MATCHED_ROUTE"]);
+            assert_eq!("/fizz/buzz", parsed_response["X_RAW_PATH_INFO"]);
+            assert_eq!("/exactparent/wildcard", parsed_response["SCRIPT_NAME"]);
+        }
+
+        {
+            let route = "/wildcardparent";
+
+            let (description, parsed_response) = get_decription_and_evs_from_standalone_bindle(bindle_id, route).await;
+            assert_eq!("This is the main entry point", description);
+            
+            assert_eq!("", parsed_response["PATH_INFO"]);
+            assert_eq!("", parsed_response["PATH_TRANSLATED"]);
+            assert_eq!("/wildcardparent/...", parsed_response["X_MATCHED_ROUTE"]);
+            assert_eq!("", parsed_response["X_RAW_PATH_INFO"]);
+            assert_eq!("/wildcardparent", parsed_response["SCRIPT_NAME"]);
+        }
+
+        {
+            let route = "/wildcardparent/fizz/buzz";
+
+            let (description, parsed_response) = get_decription_and_evs_from_standalone_bindle(bindle_id, route).await;
+            assert_eq!("This is the main entry point", description);
+            
+            assert_eq!("/fizz/buzz", parsed_response["PATH_INFO"]);
+            assert_eq!("/fizz/buzz", parsed_response["PATH_TRANSLATED"]);
+            assert_eq!("/wildcardparent/...", parsed_response["X_MATCHED_ROUTE"]);
+            assert_eq!("/fizz/buzz", parsed_response["X_RAW_PATH_INFO"]);
+            assert_eq!("/wildcardparent", parsed_response["SCRIPT_NAME"]);
+        }
+
+        {
+            let route = "/wildcardparent/exact";
+
+            let (description, parsed_response) = get_decription_and_evs_from_standalone_bindle(bindle_id, route).await;
+            assert_eq!("This is the .../exact handler", description);
+            
+            assert_eq!("", parsed_response["PATH_INFO"]);
+            assert_eq!("", parsed_response["PATH_TRANSLATED"]);
+            assert_eq!("/wildcardparent/exact", parsed_response["X_MATCHED_ROUTE"]);
+            assert_eq!("", parsed_response["X_RAW_PATH_INFO"]);
+            assert_eq!("/wildcardparent/exact", parsed_response["SCRIPT_NAME"]);
+        }
+
+        {
+            let route = "/wildcardparent/wildcard/fizz/buzz";
+
+            let (description, parsed_response) = get_decription_and_evs_from_standalone_bindle(bindle_id, route).await;
+            assert_eq!("This is the .../wildcard/... handler", description);
+            
+            assert_eq!("/fizz/buzz", parsed_response["PATH_INFO"]);
+            assert_eq!("/fizz/buzz", parsed_response["PATH_TRANSLATED"]);
+            assert_eq!("/wildcardparent/wildcard/...", parsed_response["X_MATCHED_ROUTE"]);
+            assert_eq!("/fizz/buzz", parsed_response["X_RAW_PATH_INFO"]);
+            assert_eq!("/wildcardparent/wildcard", parsed_response["SCRIPT_NAME"]);
+        }
     }
 }
